66b10e442b30e918a0dc1e31b4e4323caf3640ac
gfsgfdgf <644396139@qq.com>
2018-07-20 14:08:37 +0800

    Initial commit

---
+++ firstworld/src/main/java/com/iotimc/firstworld/FirstMain.java
@@ -0,0 +1,11 @@
+package com.iotimc.firstworld;
+
+import com.iotimc.secondworld.SecondWorld;
+
+public class FirstMain {
+    public static void main(String[] args) {
+        System.out.println("FirstWorld");
+        SecondWorld sw = new SecondWorld();
+
+    }
+}
---
+++ secondworld/src/main/java/com/iotimc/secondworld/SecondWorld.java
@@ -0,0 +1,7 @@
+package com.iotimc.secondworld;
+
+public class SecondWorld {
+    public static void main(String[] args) {
+        System.out.println("SecondWorld");
+    }
+}
